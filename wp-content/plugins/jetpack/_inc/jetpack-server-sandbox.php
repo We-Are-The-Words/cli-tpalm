2fe4580faac75042aefb3a3d9d32623828d5758b
Amandine <ape@wearethewords.com>
2021-10-01 16:36:32 +0200

    Update plugin Jetpack

--- wp-content/plugins/jetpack/_inc/jetpack-server-sandbox.php
+++ wp-content/plugins/jetpack/_inc/jetpack-server-sandbox.php
@@ -1,33 +1,37 @@
 <?php
-
 /**
  * This feature is only useful for Automattic developers.
  * It configures Jetpack to talk to staging/sandbox servers
  * on WordPress.com instead of production servers.
+ *
+ * @package automattic/jetpack
  */
 
 /**
- * @param string $sandbox Sandbox domain
- * @param string $url URL of request about to be made
- * @param array  $headers Headers of request about to be made
- * @return array [ 'url' => new URL, 'host' => new Host ]
+ * Provides sandbox request parameters.
+ *
+ * @param string $sandbox Sandbox domain.
+ * @param string $url URL of request about to be made.
+ * @param array  $headers Headers of request about to be made.
+ * @return array [ 'url' => new URL, 'host' => new Host ].
  */
 function jetpack_server_sandbox_request_parameters( $sandbox, $url, $headers ) {
 	$host = '';
 
-	$url_host = parse_url( $url, PHP_URL_HOST );
+	$url_host = wp_parse_url( $url, PHP_URL_HOST );
 
 	switch ( $url_host ) {
-	case 'public-api.wordpress.com' :
-	case 'jetpack.wordpress.com' :
-	case 'dashboard.wordpress.com' :
-		$host = isset( $headers['Host'] ) ? $headers['Host'] : $url_host;
-		$url = preg_replace(
-			'@^(https?://)' . preg_quote( $url_host, '@' ) . '(?=[/?#].*|$)@',
-			'\\1' . $sandbox,
-			$url,
-			1
-		);
+		case 'public-api.wordpress.com':
+		case 'jetpack.wordpress.com':
+		case 'jetpack.com':
+		case 'dashboard.wordpress.com':
+			$host = isset( $headers['Host'] ) ? $headers['Host'] : $url_host;
+			$url  = preg_replace(
+				'@^(https?://)' . preg_quote( $url_host, '@' ) . '(?=[/?#].*|$)@',
+				'${1}' . $sandbox,
+				$url,
+				1
+			);
 	}
 
 	return compact( 'url', 'host' );
@@ -38,8 +42,9 @@ function jetpack_server_sandbox_request_parameters( $sandbox, $url, $headers ) {
  * server specified by `JETPACK__SANDBOX_DOMAIN`.
  *
  * Attached to the `requests-requests.before_request` filter.
- * @param string &$url URL of request about to be made
- * @param array  &$headers Headers of request about to be made
+ *
+ * @param string $url URL of request about to be made.
+ * @param array  $headers Headers of request about to be made.
  * @return void
  */
 function jetpack_server_sandbox( &$url, &$headers ) {
@@ -50,11 +55,11 @@ function jetpack_server_sandbox( &$url, &$headers ) {
 	$original_url = $url;
 
 	$request_parameters = jetpack_server_sandbox_request_parameters( JETPACK__SANDBOX_DOMAIN, $url, $headers );
-	$url = $request_parameters['url'];
+	$url                = $request_parameters['url'];
 	if ( $request_parameters['host'] ) {
 		$headers['Host'] = $request_parameters['host'];
 		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-			error_log( sprintf( "SANDBOXING via '%s': '%s'", JETPACK__SANDBOX_DOMAIN, $original_url ) );
+			error_log( sprintf( "SANDBOXING via '%s': '%s'", JETPACK__SANDBOX_DOMAIN, $original_url ) ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
 		}
 	}
 }
